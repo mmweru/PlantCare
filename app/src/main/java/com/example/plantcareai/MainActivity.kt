737bdd4ae8ee942d4734fee648072803d97ac041
Maryann Mweru <mwerumaryann@gmail.com>
2024-03-28 00:20:23 +0300

    feat: fix failing CI/CD

--- app/src/main/java/com/example/plantcareai/MainActivity.kt
+++ app/src/main/java/com/example/plantcareai/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,7 +15,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Your content setup here
+            PlantCareAITheme {
+                // Your content setup here
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Greeting(name = "Android")
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello, $name!", modifier = Modifier.fillMaxSize())
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    PlantCareAITheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
         }
     }
 }
--- app/src/main/java/com/example/plantcareai/authentication/Splash.kt
+++ app/src/main/java/com/example/plantcareai/authentication/Splash.kt
@@ -2,7 +2,15 @@ package com.example.plantcareai.authentication
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
